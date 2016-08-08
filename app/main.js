cb7a7977a1602e3e1e7e38bef770c1f0fcc9fc97
Raphael Fruneaux <raphaelfruno@gmail.com>
2016-08-08 19:06:17 -0300

    create window

---
+++ app/main.js
@@ -0,0 +1,27 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('window-all-closed', function () {
+   if (process.platform != 'darwin') {
+      app.quit()
+   }
+});
+
+function createWindow () {
+   mainWindow = new BrowserWindow({width: 800, height: 600})
+   mainWindow.loadURL('file://${__dirname}/index.html')
+   mainWindow.openDevTools()
+   mainWindow.on('closed', function () {
+      mainWindow = null
+   })
+}
+
+app.on('ready', createWindow)
+app.on('activate', function () {
+   if (mainWindow === null) {
+      createWindow()
+   }
+})
